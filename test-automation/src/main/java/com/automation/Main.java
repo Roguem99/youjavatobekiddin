4e075679c424fe65d6f79a8eb1fdc4c53bb5dc78
Seth Tarrants <seth.tarrants@gmail.com>
2025-02-18 19:13:33 -0500

    updated with maven

--- test-automation/src/main/java/com/automation/Main.java
+++ test-automation/src/main/java/com/automation/Main.java
@@ -1,18 +1,15 @@
-package youjavatobekiddin;
+package com.automation;
+
 public class Main {
     static String name = "Seth";
-    static String formattedString = String.format("%s is learning Java", name);
+    static String formattedString;
     public static void main(String[] args) {
         System.out.println(String.format("Hello %s", name));
         System.out.println(formattedString);
-
         System.out.println(3);
         System.out.println(358);
         System.out.println(50000);
-        System.out.println(3 + 3);
-        System.out.println(2 * 5);
-
-
+        System.out.println(6);
+        System.out.println(10);
     }
-
 }
